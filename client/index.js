cd850c4ebc7dc52d93357e1eda23fbd36e75097d
vipul-khedekar <3kvipul@gmail.com>
2022-10-28 18:03:10 +0530

    Update: Cleanup feature to close menu when a link is clicked. Sectioned areas for cleaner code.

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
+//ELEMENT SELECTION
 const navMenu = document.querySelector(`[data-nav-menu]`);
 const navToggle = document.querySelector(`[data-nav-toggle]`);
 const navClose = document.querySelector(`[data-nav-close]`);
+const navLink = document.querySelectorAll(`[data-nav-link]`);
 
-//MENU TOGGLE FOR MOBILE VIEW
-//Queried elements are first validated to avoid undefined/blank
+//MENU TOGGLE - MOBILE VIEW
+//queried elements are first validated to avoid undefined/blank
 if (navToggle) {
   navToggle.addEventListener(`click`, () => {
     navMenu.classList.add(`show-menu`);
@@ -13,5 +15,14 @@ if (navToggle) {
 if (navClose) {
   navClose.addEventListener(`click`, () => {
     navMenu.classList.remove(`show-menu`);
+  });
+}
+
+//CLOSE MENU WHEN A LINK IS CLICKED - MOBILE VIEW
+if (navLink.length) {
+  navLink.forEach((link) => {
+    link.addEventListener(`click`, () => {
+      navMenu.classList.remove(`show-menu`);
+    });
   });
 }
