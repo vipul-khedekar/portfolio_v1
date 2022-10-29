a63da9e1c57cd21f2d40c093721ce69aedda679c
vipul-khedekar <3kvipul@gmail.com>
2022-10-29 19:20:33 +0530

    Update: Feature to slide potfolio and detect first and last slide.

--- client/index.js
+++ client/index.js
@@ -3,6 +3,10 @@ const navMenu = document.querySelector(`[data-nav-menu]`);
 const navToggle = document.querySelector(`[data-nav-toggle]`);
 const navClose = document.querySelector(`[data-nav-close]`);
 const navLink = document.querySelectorAll(`[data-nav-link]`);
+const sliderWrapper = document.querySelector(`[data-slider-wrapper]`);
+const sliderSlides = document.querySelectorAll(`[data-slider-slides]`);
+const sliderPrev = document.querySelector(`[data-slider-prev]`);
+const sliderNext = document.querySelector(`[data-slider-next]`);
 
 //MENU TOGGLE - MOBILE VIEW
 //queried elements are first validated to avoid undefined/blank
@@ -24,5 +28,31 @@ if (navLink.length) {
     link.addEventListener(`click`, () => {
       navMenu.classList.remove(`show-menu`);
     });
+  });
+}
+
+//PORTFOLIO SLIDER
+let counter = 0;
+const size = sliderSlides[0].clientWidth;
+
+if (sliderPrev) {
+  sliderPrev.addEventListener(`click`, () => {
+    if (counter <= 0) {
+      return;
+    }
+    sliderWrapper.style.transition = `transform 0.4s ease-in-out`;
+    counter--;
+    sliderWrapper.style.transform = `translateX(${-size * counter}px)`;
+  });
+}
+
+if (sliderNext) {
+  sliderNext.addEventListener(`click`, () => {
+    if (counter >= 3) {
+      return;
+    }
+    sliderWrapper.style.transition = `transform 0.4s ease-in-out`;
+    counter++;
+    sliderWrapper.style.transform = `translateX(${-size * counter}px)`;
   });
 }
